0c1a61660f1056dbd30f18fd2ef3e2bbf59e5846
Bhavin Prajapati <bhavin.prajapati@tatvasoft.com>
2023-01-04 09:58:31 +0530

    project setup

---
+++ vr-theme-6/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Button from 'react-bootstrap/esm/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+        <Button variant="primary">Primary</Button>
+    </div>
+  );
+}
+
+export default App;
