5229af3e94213835305e18446603c88d67346c5b
Mihir3070 <93573109+Mihir3070@users.noreply.github.com>
2023-01-05 10:19:49 +0530

    inner changes3

--- vr-theme-6/src/Pages/Innerpage/index.tsx
+++ vr-theme-6/src/Pages/Innerpage/index.tsx
@@ -10,7 +10,8 @@ import Form from "react-bootstrap/Form";
 import { alertErrorIcon, alertInfoIcon, alertSuccessIcon, alertWarningIcon, hidePasswordIcon, uploadIcon } from "../../assets/images";
 import { Alert } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
-
+import Tabs from "react-bootstrap/esm/Tabs";
+import Tab from 'react-bootstrap/Tab';
 
 
 const Innerpage = () => {
@@ -22,7 +23,7 @@ const Innerpage = () => {
       <div className="wrapper">
         <Header />
         <Sidebar />
-        <div className="main-content">
+        <div className="main-content inner">
           <h3 className="page-title">Users</h3>
           <div className="custom-card">
             {/* -------- first column start ---------  */}
@@ -30,12 +31,12 @@ const Innerpage = () => {
             <FloatingLabel
               controlId="floatingInput"
               label="Username"
-              className="mb-3"
+
             >
               <Form.Control type="text" placeholder="Username" />
             </FloatingLabel>
 
-            <FloatingLabel label="Dropdown" className="mb-3">
+            <FloatingLabel label="Dropdown" >
               <Form.Select>
                 {/* <option>Drop Down</option> */}
                 <option value="1">One</option>
@@ -47,7 +48,7 @@ const Innerpage = () => {
             <FloatingLabel
               controlId="multilineInput"
               label="Multiple  line"
-              className="mb-3"
+
             >
               <Form.Control type="text" placeholder="Multiple line" as="textarea" />
             </FloatingLabel>
@@ -60,7 +61,7 @@ const Innerpage = () => {
                 <FloatingLabel
                   controlId="floatingInput"
                   label="Username"
-                  className="mb-3"
+
                 >
                   <Form.Control type="text" placeholder="Username" />
                 </FloatingLabel>
@@ -69,7 +70,7 @@ const Innerpage = () => {
                 <FloatingLabel
                   controlId="floatingPassword"
                   label="Password"
-                  className="password-wrapper"
+                  className="password-wrapper "
                 >
                   <Form.Control type="password" placeholder="Password" />
                   <button className="btn icon-wrapper">
@@ -81,7 +82,7 @@ const Innerpage = () => {
                 <FloatingLabel
                   controlId="innerName"
                   label="Name"
-                  className="mb-3"
+
                 >
                   <Form.Control type="text" placeholder="Name" />
                 </FloatingLabel>
@@ -90,7 +91,7 @@ const Innerpage = () => {
                 <FloatingLabel
                   controlId="innerError"
                   label="Error"
-                  className="mb-3"
+
                 >
                   <Form.Control type="text" placeholder="Error" value="Web Designer" />
                 </FloatingLabel>
@@ -105,7 +106,7 @@ const Innerpage = () => {
                 <FloatingLabel
                   controlId="floatingFocus"
                   label="Focus"
-                  className="mb-3"
+
                 >
                   <Form.Control type="text" placeholder="Focus" />
                 </FloatingLabel>
@@ -115,14 +116,14 @@ const Innerpage = () => {
                 <FloatingLabel
                   controlId="disabled"
                   label="Disabled"
-                  className="mb-3"
+
                 >
                   <Form.Control type="text" placeholder="Disabled" value="John Doe" disabled />
                 </FloatingLabel>
               </Col>
 
               <Col lg={4} md={6}>
-                <FloatingLabel label="Dropdown" className="mb-3">
+                <FloatingLabel label="Dropdown" >
                   <Form.Select>
                     {/* <option>Drop Down</option> */}
                     <option value="1">One</option>
@@ -141,7 +142,7 @@ const Innerpage = () => {
                 <FloatingLabel
                   controlId="floatingInputSecond"
                   label="Username"
-                  className="mb-3"
+
                 >
                   <Form.Control type="text" placeholder="Username" />
                 </FloatingLabel>
@@ -151,7 +152,7 @@ const Innerpage = () => {
                 <FloatingLabel
                   controlId="floatingPassword"
                   label="Password"
-                  className="password-wrapper"
+                  className="password-wrapper "
                 >
                   <Form.Control type="password" placeholder="Password" />
                   <button className="btn icon-wrapper">
@@ -161,7 +162,7 @@ const Innerpage = () => {
               </Col>
 
               <Col lg={3} md={6}>
-                <FloatingLabel label="Dropdown" className="mb-3">
+                <FloatingLabel label="Dropdown" >
                   <Form.Select>
                     {/* <option>Drop Down</option> */}
                     <option value="1">One</option>
@@ -175,7 +176,7 @@ const Innerpage = () => {
                 <FloatingLabel
                   controlId="innerErrorSecond"
                   label="Error"
-                  className="mb-3"
+
                 >
                   <Form.Control type="text" placeholder="Error" value="Web Designer" />
                 </FloatingLabel>
@@ -185,88 +186,118 @@ const Innerpage = () => {
 
 
             {/* -------- other design start ---------  */}
-            <h5>Other Design elements</h5>
-
-            <h6>File Upload</h6>
-            <div className="file-upload-main">
-              <input type="file" name="Upload" id="upload" placeholder="Upload" />
-              <div className="file-content">
-                <img src={uploadIcon} alt="Upload Icon" />
-                <h4>Upload</h4>
+            <div className="other-design">
+              <h5>Other Design elements</h5>
+              <h6>File Upload</h6>
+              <div className="file-upload-main">
+                <input type="file" name="Upload" id="upload" placeholder="Upload" />
+                <div className="file-content">
+                  <img src={uploadIcon} alt="Upload Icon" />
+                  <h4>Upload</h4>
+                </div>
+                <p>No item Selected</p>
               </div>
-              <p>No item Selected</p>
-            </div>
 
-            <h6>Check Box</h6>
-            <div className="mb-3">
-              <Form.Check
-                inline
-                label="Selected"
-                name="group1"
-                type={"checkbox"}
-                id={`inline-check-1`}
-              />
-              <Form.Check
-                inline
-                label="Not Yet Selected"
-                name="group1"
-                type={"checkbox"}
-                id={`inline-check-2`}
-              />
-            </div>
+              <h6>Check Box</h6>
+              <div className="check-group">
+                <Form.Check
+                  inline
+                  label="Selected"
+                  name="group1"
+                  type={"checkbox"}
+                  id={`inline-check-1`}
+                />
+                <Form.Check
+                  inline
+                  label="Not Yet Selected"
+                  name="group1"
+                  type={"checkbox"}
+                  id={`inline-check-2`}
+                />
+              </div>
 
-            <h6>Radio Button</h6>
-            <div className="mb-3">
-              <Form.Check
-                inline
-                label="Selected"
-                name="group1"
-                type={"radio"}
-                id={`inline-radio-1`}
-              />
-              <Form.Check
-                inline
-                label="Not Yet Selected"
-                name="group1"
-                type={"radio"}
-                id={`inline-radio-2`}
-              />
-            </div>
+              <h6>Radio Button</h6>
+              <div className="check-group">
+                <Form.Check
+                  inline
+                  label="Selected"
+                  name="group1"
+                  type={"radio"}
+                  id={`inline-radio-1`}
+                />
+                <Form.Check
+                  inline
+                  label="Not Yet Selected"
+                  name="group1"
+                  type={"radio"}
+                  id={`inline-radio-2`}
+                />
+              </div>
 
-            <h6>Snack Bar</h6>
-            <Row>
-              <Col md={6}>
-                <Alert variant="danger" dismissible>
-                  <img src={alertErrorIcon} className="alert-icon" alt="Error Icon" />
-                  <p>This is an error Message</p>
-                </Alert>
-              </Col>
-              <Col md={6}>
-                <Alert variant="info" dismissible>
-                  <img src={alertInfoIcon} className="alert-icon" alt="Info Icon" />
-                  <p>This is an information message!</p>
-                </Alert>
-              </Col>
-              <Col md={6}>
-                <Alert variant="warning" dismissible>
-                  <img src={alertWarningIcon} className="alert-icon" alt="Warning Icon" />
-                  <p>This is a warning message!</p>
-                </Alert>
-              </Col>
-              <Col md={6}>
-                <Alert variant="success" dismissible>
-                  <img src={alertSuccessIcon} className="alert-icon" alt="Success Icon" />
-                  <p>This is a Success message!</p>
-                </Alert>
-              </Col>
-            </Row>
+              <h6>Snack Bar</h6>
+              <Row className="snackbar-group">
+                <Col md={6}>
+                  <Alert variant="danger" dismissible>
+                    <img src={alertErrorIcon} className="alert-icon" alt="Error Icon" />
+                    <p>This is an error Message</p>
+                  </Alert>
+                </Col>
+                <Col md={6}>
+                  <Alert variant="info" dismissible>
+                    <img src={alertInfoIcon} className="alert-icon" alt="Info Icon" />
+                    <p>This is an information message!</p>
+                  </Alert>
+                </Col>
+                <Col md={6}>
+                  <Alert variant="warning" dismissible>
+                    <img src={alertWarningIcon} className="alert-icon" alt="Warning Icon" />
+                    <p>This is a warning message!</p>
+                  </Alert>
+                </Col>
+                <Col md={6}>
+                  <Alert variant="success" dismissible>
+                    <img src={alertSuccessIcon} className="alert-icon" alt="Success Icon" />
+                    <p>This is a Success message!</p>
+                  </Alert>
+                </Col>
+              </Row>
 
-            <h6>CTA</h6>
-            <div className="btn-groups">
-              <Button variant="primary">Primary</Button>
-              <Button variant="outline-primary">Secondary</Button>
+              <h6>CTA</h6>
+              <div className="btn-groups">
+                <Button variant="primary">Primary</Button>
+                <Button variant="outline-primary">Secondary</Button>
+              </div>
             </div>
             {/* -------- other design end ---------  */}
+
+
+            {/* -------- Tabs start ---------  */}
+            <h5>Tabs</h5>
+            {/* <Tabs
+              defaultActiveKey="home"
+              id="uncontrolled-tab-example"
+              
+            >
+              <Tab eventKey="home" title="Home">
+                It is a long established fact that a reader will be distracted by the readable content of
+                a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less
+                normal distribution of letters, as opposed to using 'Content here, content here', making it look
+                like readable English. Many desktop publishing
+                packages and web page editors now use Lorem Ipsum
+                as their default model text, and a search for 'lorem ipsum' will
+                uncover many web sites still in their infancy.
+              </Tab>
+              <Tab eventKey="profile" title="Profile">
+                Tab Content 2
+              </Tab>
+              <Tab eventKey="contact" title="Contact">
+                It is a long established fact that a reader will be distracted by the readable content of
+                a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less
+                normal distribution of letters, as opposed to using 'Content here, content here', making it look
+                like readable English. Many desktop publishing
+              </Tab>
+            </Tabs> */}
+            {/* -------- Tabs send ---------  */}
           </div>
         </div>
       </div>
